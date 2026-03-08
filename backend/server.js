fed42ef4fed80be95772fe2093d4aa2cedd115dc
Givi <givisab12345@gmail.com>
2026-03-08 13:44:54 +0100

    database migration, from local to online hosted

--- backend/server.js
+++ backend/server.js
@@ -16,11 +16,8 @@ app.use(express.json());
 
 // 1. Connect to PostgreSQL
 const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false },
 });
 
 // Test connection immediately
